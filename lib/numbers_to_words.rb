09c606b378b43ba294801994adbd85e3829cf053
Krystan Menne and Mike Mahoney <kmenne@gmail.com>
2016-01-05 13:10:42 -0800

    return a written number for a two digit number

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -6,12 +6,28 @@ class Fixnum
           15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen",
           19 => "nineteen"}
 
+  tens = {2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty",
+          6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety"}
+
   define_method(:numbers_to_words) do
-    ones_to_words
+    digits = self.to_s.split('').map { |digit| digit.to_i }
+    words = []
+    if (self < 20)
+      ones_to_words
+    elsif (self < 100)
+      tens_to_words(digits, words)
+    else
+      "under construction"
+    end
   end
 
   define_method(:ones_to_words) do
     ones.fetch(self)
   end
 
+  define_method(:tens_to_words) do |digits, words|
+    words.push(tens.fetch(digits[0]))
+    words.push(ones.fetch(digits[1])) unless digits[1] == 0
+    words.join(" ")
+  end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -10,4 +10,12 @@ describe('Fixnum#numbers_to_words') do
   it("returns a written number for a number between ten and twenty") do
     expect(13.numbers_to_words()).to(eq("thirteen"))
   end
+
+  it("returns a written number for a two digit number") do
+    expect(42.numbers_to_words()).to(eq("forty two"))
+  end
+
+  it("returns a written number for a two digit number") do
+    expect(40.numbers_to_words()).to(eq("forty"))
+  end
 end
