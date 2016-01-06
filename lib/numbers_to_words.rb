19e723e7a8d87928a96878234c2e163e9d8bbc20
Krystan Menne and Mike Mahoney <mahonmr@me.com>
2016-01-05 16:27:56 -0800

    remove redundant ones_to_words method

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -2,10 +2,8 @@ class Fixnum
   define_method(:numbers_to_words) do
     digits = self.to_s.split('').map { |digit| digit.to_i }
     words = []
-    if (self < 20)
-      digits.ones_to_words()
-    elsif (self < 100)
-      digits.tens_to_words(words)
+    if (self < 100)
+      digits.ones_and_tens(words)
     elsif (self < 1000)
       digits.hundreds_to_words(words)
     elsif (self < 100000)
@@ -26,11 +24,7 @@ class Array
   tens = {2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty",
           6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety"}
 
-  define_method(:ones_to_words) do
-    ones.fetch(self.join.to_i)
-  end
-
-  define_method(:tens_to_words) do |words|
+  define_method(:ones_and_tens) do |words|
     tens_place = self.join.to_i
     if tens_place > 19
       words.push(tens.fetch(self[0]))
@@ -42,12 +36,13 @@ class Array
   end
 
   define_method(:hundreds_to_words) do |words|
-    words.push(self.slice!(0, self.length - 2).ones_to_words() + " hundred")
-    self.tens_to_words(words)
+    self.slice!(0, self.length - 2).ones_and_tens(words)
+    words.push("hundred")
+    self.ones_and_tens(words)
   end
 
   define_method(:thousands_to_words) do |words|
-    self.slice!(0, self.length - 3).tens_to_words(words)
+    self.slice!(0, self.length - 3).ones_and_tens(words)
     words.push("thousand")
     self.hundreds_to_words(words)
   end
